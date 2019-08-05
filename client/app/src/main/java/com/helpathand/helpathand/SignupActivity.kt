ee5ef669674a09166a56fcefc3784beeeb654d4f
kaleempeeroo <kpeeroo@gmail.com>
2019-08-05 06:54:12 +0400

    Attempt to login with Auth0

--- client/app/src/main/java/com/helpathand/helpathand/SignupActivity.kt
+++ client/app/src/main/java/com/helpathand/helpathand/SignupActivity.kt
@@ -1,4 +1,5 @@
 package com.helpathand.helpathand
+import android.app.PendingIntent.getActivity
 import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
@@ -8,14 +9,16 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import android.widget.Toast.makeText
 import com.helpathand.helpathand.R
 import com.auth0.android.Auth0
+import com.auth0.android.lock.AuthenticationCallback
 import com.auth0.android.lock.utils.LockException
 import com.auth0.android.lock.LockCallback
-import com.auth0.android.lock.AuthenticationCallback
 import com.auth0.android.result.Credentials
 //import android.R
 import com.auth0.android.lock.Lock
+import com.auth0.android.provider.AuthCallback
 
 
 class SignupActivity : AppCompatActivity() {
@@ -25,15 +28,17 @@ class SignupActivity : AppCompatActivity() {
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        //setContentView(R.layout.activity_signup)
         var auth0_account = Auth0(Constants.AUTH0_CLIENT_ID, Constants.AUTH0_DOMAIN)
         //Configure the account in OIDC conformant mode
         //Resolves login legacy endpoints
         auth0_account.isOIDCConformant = true
         //Use the account to launch Lock
-        lock = Lock.newBuilder(auth0_account, callback)
+
+        lock = Lock.newBuilder(auth0_account, callback).withScheme("com.helpathand.helpathand")
             .withAudience("https://${Constants.AUTH0_DOMAIN}/userinfo")
-            .build(this);
+            .build(this)
+
         startActivity(lock!!.newIntent((this)))
 
     }
@@ -44,23 +49,27 @@ class SignupActivity : AppCompatActivity() {
         lock = null
     }
 
-    // Callback handle from auth0 after client authentication
-    private val callback = object : AuthenticationCallback() {
-        override fun onAuthentication(credentials: Credentials?) {
-            //Authenticated
-        }
+// Callback handle from auth0 after client authentication
+val callback  = object : AuthenticationCallback() {
+    override fun onAuthentication(credentials: Credentials?) {
+        //Authenticated
+        Toast.makeText(applicationContext,"Authentication Success", Toast.LENGTH_SHORT).show()
 
-        override fun onCanceled() {
-            //User pressed back
-        }
-
-
-        override fun onError(error: LockException?) {
-            //Exception occurred
-
-        }
 
     }
+
+    override fun onCanceled() {
+        //User pressed back
+    }
+
+
+    override fun onError(error: LockException?) {
+        //Exception occurred
+        Log.e(TAG,error!!.message)
+
+    }
+
+}
 
     companion object {
         private val TAG = "SignupActivity"
